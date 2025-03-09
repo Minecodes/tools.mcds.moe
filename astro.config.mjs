9da41b4ee87a0dba858ab99b896070a6da1cf871
Thies Ohme <contact@minecodes.de>
2025-03-09 21:24:13 +0100

    [v1.0] 3e13df0a04505528634cbce22ca5ceb8700c01e2

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,15 @@ import partytown from '@astrojs/partytown';
 
 import expressiveCode from 'astro-expressive-code';
 
+import compressor from 'astro-compressor';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tools.mcds.moe',
-  integrations: [expressiveCode(), mdx(), sitemap(), partytown()],
+  integrations: [expressiveCode(), mdx(), sitemap(), partytown(), compressor({
+    brotli: true,
+    gzip: true,
+  })],
 
   vite: {
     plugins: [tailwindcss()],
